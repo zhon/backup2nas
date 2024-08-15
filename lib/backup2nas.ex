16634a587624b5aa1f9c48e2cb0ed6f097c93ba9
Zhon <zhon@xmission.com>
2024-08-15 15:15:46 -0600

    added walk_directory

--- lib/backup2nas.ex
+++ lib/backup2nas.ex
@@ -8,11 +8,30 @@ defmodule Backup2nas do
 
   ## Examples
 
-      iex> Backup2nas.hello()
-      :world
 
   """
-  def hello do
-    :world
+  def walk_directory(start_path) do
+    walk_directory(start_path, [])
   end
+
+  defp walk_directory(path, acc) do
+    case File.ls(path) do
+      {:ok, entries} ->
+        directories =
+          entries
+          |> Enum.map(&Path.join(path, &1))
+          |> Enum.filter(&File.dir?/1)
+
+        # Recur on all subdirectories first (depth-first)
+        Enum.reduce(directories, acc, fn dir, acc -> 
+          walk_directory(dir, acc)
+        end)
+        # Finally, add the current directory to the accumulator
+        |> Kernel.++([path])
+
+      {:error, _reason} ->
+        acc
+    end
+  end
+
 end
--- test/backup2nas_test.exs
+++ test/backup2nas_test.exs
@@ -2,7 +2,18 @@ defmodule Backup2nasTest do
   use ExUnit.Case
   doctest Backup2nas
 
-  test "greets the world" do
-    assert Backup2nas.hello() == :world
+  describe "finding directories" do
+
+    test "finds directories in order" do
+      assert Enum.to_list(Backup2nas.walk_directory("./test/dir")) ==
+        [
+           "./test/dir/subdir2",
+           "./test/dir/subdir1/subsubdir",
+           "./test/dir/subdir1",
+           "./test/dir",
+        ]
+
+    end
+
   end
 end
